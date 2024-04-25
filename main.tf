d94f9b4534c191c1eff34b1b64b416b7f0c0818c
Rick Ryan <rick.ryan2@outlook.com>
2024-04-25 16:32:30 +0100

    feat: add vnet for resource group

--- main.tf
+++ main.tf
@@ -20,3 +20,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "myTFResourceGroup"
   location = "eastus"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTFVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
