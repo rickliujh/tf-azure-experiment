7041d3d38f448229355ba49e2a5878f42eaeaa1d
Rick Ryan <rick.ryan2@outlook.com>
2024-05-03 23:55:25 +0100

    chore(requester): organize providers to main.tf

--- k8s/requester/main.tf
+++ k8s/requester/main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/random"
       version = "3.6.1"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.29.0"
+    }
   }
 
   required_version = ">= 1.1.0"
@@ -20,9 +24,21 @@ terraform {
   }
 }
 
+locals {
+  kube_config = yamldecode(azurerm_kubernetes_cluster.tfk8salpha.kube_config_raw)
+}
+
 provider "azurerm" {
   features {}
 }
 
 provider "random" {}
+
+provider "kubernetes" {
+  host = local.kube_config.clusters[0].cluster.server
+
+  client_key             = base64decode(local.kube_config.users[0].user.client-key-data)
+  client_certificate     = base64decode(local.kube_config.users[0].user.client-certificate-data)
+  cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
+}
 
--- k8s/requester/workload.tf
+++ k8s/requester/workload.tf
@@ -1,15 +1,3 @@
-locals {
-  kube_config = yamldecode(azurerm_kubernetes_cluster.tfk8salpha.kube_config_raw)
-}
-
-provider "kubernetes" {
-  host = local.kube_config.clusters[0].cluster.server
-
-  client_key             = base64decode(local.kube_config.users[0].user.client-key-data)
-  client_certificate     = base64decode(local.kube_config.users[0].user.client-certificate-data)
-  cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
-}
-
 resource "kubernetes_namespace" "ns_requester" {
   metadata {
     name = "requester"
