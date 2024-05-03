0d7c5eb3cfbeac2c896692857e30f69b7c9595ca
Rick Ryan <rick.ryan2@outlook.com>
2024-05-03 23:38:08 +0100

    refactor(requester): files reorganized

---
+++ k8s/requester/aks.tf
@@ -0,0 +1,61 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}k8s-ex" # k8s for experiment
+  location = var.location
+
+  tags = var.tags
+}
+
+resource "random_id" "workspace" {
+  keepers = {
+    # Generate a new id each time we switch to a new resource group
+    group_name = azurerm_resource_group.rg.name
+  }
+
+  byte_length = 8
+}
+
+resource "azurerm_log_analytics_workspace" "logws" {
+  name                = "${var.prefix}-k8slogws-${random_id.workspace.hex}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "PerGB2018"
+}
+
+resource "azurerm_log_analytics_solution" "logsol" {
+  solution_name         = "ContainerInsights"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  workspace_resource_id = azurerm_log_analytics_workspace.logws.id
+  workspace_name        = azurerm_log_analytics_workspace.logws.name
+
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/ContainerInsights"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "tfk8salpha" {
+  name                = "${var.prefix}k8s-alpha"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "${var.prefix}k8s-alpha"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+    max_pods   = 250
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  oms_agent {
+    log_analytics_workspace_id      = azurerm_log_analytics_workspace.logws.id
+    msi_auth_for_monitoring_enabled = true
+  }
+
+  tags = var.tags
+}
+
