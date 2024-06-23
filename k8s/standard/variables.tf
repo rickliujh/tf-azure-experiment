789d367dad6f534ceb003adf973e2cca30e45c92
Rick Ryan <rick.ryan2@outlook.com>
2024-06-23 15:03:56 +0100

    feat: standard setup for test

---
+++ k8s/standard/variables.tf
@@ -0,0 +1,40 @@
+variable "prefix" {
+  type        = string
+  default     = "tf-"
+  nullable    = false
+  description = "the prefix will apply to all resource name"
+}
+
+variable "location" {
+  type        = string
+  default     = "eastus"
+  description = "resource location"
+}
+
+variable "sp" {
+  type = object({
+    app_id          = string,
+    display_name    = string,
+    password        = string,
+    tenant          = string,
+    subscription_id = string,
+  })
+  default     = null
+  description = "Azure Service Principal"
+  nullable    = true
+  sensitive   = true
+}
+
+variable "tags" {
+  type = object({
+    of    = string,
+    usage = string,
+  })
+  default = {
+    of    = "tf"
+    usage = "test"
+  }
+  description = "common tags to mark resource belonging"
+  nullable    = true
+}
+
